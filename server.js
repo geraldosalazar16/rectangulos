2efbf1a65a89dbc00b8b3473114223cfcb2e56e8
geraldosalazar16 <34693854+geraldosalazar16@users.noreply.github.com>
2018-02-27 17:49:21 +0100

    p

--- server.js
+++ server.js
@@ -22,8 +22,8 @@ app.use(body_parser.urlencoded({extended:true}));
 app.use(express.static(__dirname + '/public'));
 app.use( body_parser.json({ type: 'application/json' }) );
 
-var port = process.env.PORT || 8080;
-app.listen(port_number, function(){
+var port = process.env.PORT || 3000;
+app.listen(port, function(){
   console.log('servidor escuchando en puerto 3000');
 });
 //var calculo = require('./routes/calculo');
